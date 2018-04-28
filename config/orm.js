24f4306be8e1489d804f3f42be30232afea86fd2
projectkross <projectkross@yahoo.com>
2018-04-27 22:38:36 -0400

    Homework

---
+++ config/orm.js
@@ -0,0 +1,53 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES ('";
+    queryString += vals
+    queryString += "'); ";
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  updateOne: function(table, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET devoured = 1 WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("FourNine Burger");
+INSERT INTO burgers (burger_name) VALUES ("Layercheese Burgertown");
+INSERT INTO burgers (burger_name) VALUES ("Roll of Cheesey");

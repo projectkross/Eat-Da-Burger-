d39d7c51249ecf402eb383a5b59cc073a141ef54
projectkross <projectkross@yahoo.com>
2018-04-28 23:09:48 -0400

    Update

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Deathwing13',
+    password: "Deathwing13",
     database: 'burgers_db'
   });
 
